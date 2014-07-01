d765442eb2b3aa53cbf346c99e8bbd06be425f06
Ryan <drlucas@rogers.com>
2014-07-01 17:30:06 -0400

    Update fpp_install.sh

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-/bin/pwd >> /tmp/you
 pushd $(dirname $(which $0))
 
+cp pigpiod /etc/init.d
+sudo chmod 755 /etc/init.d/pigpiod 
+
+
 /usr/bin/wget abyz.co.uk/rpi/pigpio/pigpio.zip
-/bin/pwd >> /tmp/me
 unzip pigpio.zip 
 cd PIGPIO/
 make
 make install 
-cp pigpiod /etc/init.d
-sudo chmod 755 /etc/init.d/pigpiod 
+
 cd /etc/init.d
 update-rc.d pigpiod defaults 99
 complete -W "$(ls /etc/init.d/)" service
+
+
 service pigpiod start
 
 
