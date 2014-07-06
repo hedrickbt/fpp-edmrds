a674780923b549647b5bc68e1f6cd759a3a5d6a3
Ryan <drlucas@rogers.com>
2014-07-06 19:14:21 -0400

    Updated plugin to display station ID from setup

--- plugin_setup.php
+++ plugin_setup.php
@@ -16,6 +16,7 @@ unset($output);
 <fieldset>
 <legend>EDM-LCD-RDS-EP Support Instructions</legend>
 
+
 <p>Before you use the RDS capabilities of your EDM FM transmitter you must be comfortable 
 with soldering and connect the SCL and SDA pins from the RDS chip located within the EDM FM transmitter 
 to two pins on the raspberry Pi. Currently the two PINs to use are pin 31 and 30 for SCL and SDA respectively. <br><br>
@@ -23,12 +24,12 @@ See http://www.raspberrypi-spy.co.uk/2012/09/raspberry-pi-p5-header/ for more in
 
 <br>
 Configuration of the RDS settings for the EDM transmitter can be found here: http://www.edmdesign.com/docs/EDM-TX-RDS.pdf.
- Information on the RDS chip inside the EDM transmitter can be found here: http://pira.cz/rds/mrds192.pdf
+Information on the RDS chip inside the EDM transmitter can be found here: http://pira.cz/rds/mrds192.pdf
 . Once this connection is made than you can read and set the RDS information on the unit.
 </p>
 
 <p>When you create your MP3 and OGG files, make sure you tag them with Artist and Title fields. You can upload the MP3s and OGG files through the
- File Manager in the Content Setup menu. Once the tags are set, this plug in will automatically update the RDS text when the file is played!</p>
+File Manager in the Content Setup menu. Once the tags are set, this plug in will automatically update the RDS text when the file is played!</p>
 
 <p>Known Issues:
 <ul>
@@ -42,10 +43,15 @@ Planned Features:
 <li>Set the EDM station name
 </ul>
 
+Station ID: 
+<?php 
+system('python /opt/fpp/plugins/edmrds/rds-song.py -l', $stationid);
+?>
+
+
 <p>To report a bug, please file it against the fpp-vastfmt plugin project here:
 <a href="https://github.com/drlucas/fpp-edmrds/issues/new" target="_new">fpp-edmrds GitHub Issues</a></p>
 
 </fieldset>
 </div>
 <br />
-
