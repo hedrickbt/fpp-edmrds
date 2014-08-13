120a2718e30aaf1840d896176486b84cad571e33
Ryan <drlucas@rogers.com>
2014-08-13 06:14:20 -0400

    make res-executable

--- fpp_install.sh
+++ fpp_install.sh
@@ -5,6 +5,7 @@ pushd $(dirname $(which $0))
 cp pigpiod /etc/init.d
 sudo chmod 755 /etc/init.d/pigpiod 
 sudo chmod +x /opt/fpp/plugins/edmrds/callbacks
+sudo chmod +x /opt/fpp/plugins/edmrds/rds-song.py 
 
 /usr/bin/wget abyz.co.uk/rpi/pigpio/pigpio.zip
 unzip pigpio.zip 
