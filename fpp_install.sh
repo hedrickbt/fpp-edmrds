ee331ca10e1e660c54b412906b6c010e21ab6716
Ryan <drlucas@rogers.com>
2014-10-07 21:52:24 -0400

    Updated to deal with new version of pigpio.py
    locked down using v22 instead of latest version of pigpio

--- fpp_install.sh
+++ fpp_install.sh
@@ -7,9 +7,11 @@ sudo chmod 755 /etc/init.d/pigpiod
 sudo chmod +x /opt/fpp/plugins/edmrds/callbacks
 sudo chmod +x /opt/fpp/plugins/edmrds/rds-song.py 
 
-/usr/bin/wget abyz.co.uk/rpi/pigpio/pigpio.zip
+/usr/bin/wget http://abyz.co.uk/rpi/pigpio/VERSIONS/pigpio-V22.zip
+sudo move pigpio-V22.zip pigpio.zip
 unzip pigpio.zip 
 cd PIGPIO/
+sudo sed  '2619s/data/str(data)/' -i pigpio.py
 make
 make install 
 
