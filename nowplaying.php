5f4e9c807bcfee16231446de99a4e3f68f9490ad
Ryan <drlucas@rogers.com>
2015-01-10 14:23:09 -0500

    Update nowplaying.php
    /home/pi/media/plugins/edmrds directory change

--- nowplaying.php
+++ nowplaying.php
@@ -1,7 +1,7 @@
 <?php 
   echo 'Now Playing:';
-  echo exec("python /opt/fpp/plugins/edmrds/rds-song.py -n 2>&1 ");
+  echo exec("python /home/pi/media/plugins/edmrds/rds-song.py -n 2>&1 ");
   echo '<br>';
   echo 'Station ID:';
-  echo exec("python /opt/fpp/plugins/edmrds/rds-song.py -l 2>&1 ");
+  echo exec("python /home/pi/media/plugins/edmrds/rds-song.py -l 2>&1 ");
 ?>
