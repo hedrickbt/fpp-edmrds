8618bc9b26635207376734e83ce4a291cac042c9
Ryan <drlucas@rogers.com>
2015-11-02 08:52:11 -0500

    Update plugin_setup.php
    home directory updated

--- plugin_setup.php
+++ plugin_setup.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit']))
         }
         else
         {
-    echo exec("python /home/pi/media/plugins/edmrds/rds-song.py -c $name 2>&1 ");
+    echo exec("python /home/fpp/media/plugins/edmrds/rds-song.py -c $name 2>&1 ");
         }
 }
 ?>
