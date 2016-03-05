1abb1264d185bd7a8cb6546d88325e9b62d614bd
Ryan <drlucas@rogers.com>
2016-03-04 23:36:52 -0500

    Update and rename callbacks to callbacks.py
    Use argparse vs sed for dealing with callback script

---
+++ callbacks.py
@@ -0,0 +1,51 @@
+bb_I2C.py  fpp_install.sh  pigpio.zip       rds-song.py
+fpp@FPP1 ~/media/plugins/edmrds $ cat callbacks.py 
+#!/usr/bin/env python
+
+import time
+import os
+import argparse   
+import sys
+import json
+import subprocess
+
+class Logger(object):
+    def __init__(self, filename="/edm.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+#sys.stdout = Logger("/edm.log")
+#print "Hello world !" # this is should be saved in yourlogfilename.txt
+
+parser = argparse.ArgumentParser(description='RDS Setting Application')
+parser.add_argument('-t','--type', help='Input station name (8 characters max)', required=False)
+parser.add_argument('-l','--list',help='Song name',action='store_true')
+parser.add_argument('-d','--data',help='Song name')
+args = parser.parse_args()
+
+if args.list:
+  #Tell the plugin that we should be registered for media
+   print "media"
+
+if args.type:
+   # Look for our type of plugin, doubt this is even needed at all
+   mytypearg = args.type
+   #print "My type args: %s" %mytypearg
+
+if args.data:
+   #get the json string from FPP
+   mydataarg = args.data
+   print "My data args: %s" %mydataarg
+   data = json.loads(mydataarg)
+   title = data['title'] 
+#   print "Title: %s" %title
+   os.chdir("/home/fpp/media/plugins/edmrds/")
+   directory = os.getcwd()
+#   print "Directory: %s" %directory
+   #from subprocess import call
+   #call(["rds-song.py", "-s %title"])
+   subprocess.call(['/home/fpp/media/plugins/edmrds/rds-song.py', '-s', title])
